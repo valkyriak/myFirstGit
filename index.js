e37752d3a12e071261e4e6368712e9f076f05eb2
Kane Clerke <kane.clerke@gmail.com>
2020-07-26 23:07:35 +1000

    merge conflict resolved

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 console.log("hello world")
 console.log(addition(5,10)) // Task 4
@@ -6,6 +5,4 @@ console.log(addition(5,10)) // Task 4
 function addition(x,y) {
     return x + y;
 }
-=======
 console.log("I caused a merge conflict")
->>>>>>> 29cfd829487ecedc8dc4e8a96abf13cb8dee40ed
